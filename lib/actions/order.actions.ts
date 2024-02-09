6f626aa26827f89685d66108a4bc6d49849db12c
aliasghar <aliasghar.ahb@gmail.com>
2024-02-09 11:09:26 +0330

    change order

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import Stripe from "stripe";
-
 import {
   CheckoutOrderParams,
   CreateOrderParams,
@@ -12,7 +11,7 @@ import { redirect } from "next/navigation";
 import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import Order from "../database/models/order.model";
-
+import Event from "../database/models/event.model";
 import { ObjectId } from "mongodb";
 import User from "../database/models/user.model";
 
